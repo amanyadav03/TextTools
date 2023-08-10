f70756d03f07d80dd9488c7cd1e7393e813e45fe
Aman Yadav <98152833+amanyadav03@users.noreply.github.com>
2023-08-10 21:31:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import Textform from "./Components/Textform";
-import About from "./Components/About";
+// import About from "./Components/About";
+import React, {useState} from 'react';
+
 function App() {
+  const[mode, setMode]=useState("light")
+  const toggleMode=()=>{
+    if (mode==="light")
+    {setMode("dark")}
+    else{
+    setMode("light")
+    }
+  }
   return (
     <>
-      <Navbar title="Text Tools" home="Home" about="About Us"/>
+
+      <Navbar title="Text Tools" home="Home" about="About Us" mode={mode} toggleMode={toggleMode}/>
+ 
       <div className="container my-5">
       <Textform headings='Enter your text here'/>
       </div>
+      {/* <About/> */}
     </>
-/*<About/>*/
   );
 }
 
